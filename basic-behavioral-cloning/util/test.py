9bdb7145c9a7d1648e853cd9ecbed70480184f04
ioarun <arun_kumar271@hotmail.com>
2018-11-08 18:19:07 +0530

    Add end effector positions in the dataset

--- basic-behavioral-cloning/util/test.py
+++ basic-behavioral-cloning/util/test.py
@@ -1,18 +1,33 @@
 import csv
+import intera_interface
 
-source_file = open('0.csv', 'rb')
-reader = csv.reader(source_file)
+import rospy
 
-dest_file = open('1.csv', 'w')
-writer = csv.writer(dest_file)
-writer.writerow(['right_j0_next','right_j1_next','right_j2_next','right_j3_next','right_j4_next','right_j5_next','right_j6_next'])
+rospy.init_node('test')
 
-reader.next()
-reader.next()
-reader = list(reader)
+limb = intera_interface.Limb('right')
 
-for i in range(len(reader)-1):
-	if float(reader[i][9]) == float(reader[i+1][9]):
-		writer.writerow([reader[i+1][2], reader[i+1][3], reader[i+1][4], reader[i+1][5], reader[i+1][6], reader[i+1][7], reader[i+1][8]])
-	else:
-		writer.writerow([reader[i][2], reader[i][3], reader[i][4], reader[i][5], reader[i][6], reader[i][7], reader[i][8]])
+
+def generate_next_actions():
+	source_file = open('0.csv', 'rb')
+	reader = csv.reader(source_file)
+
+	dest_file = open('1.csv', 'w')
+	writer = csv.writer(dest_file)
+	writer.writerow(['right_j0_next','right_j1_next','right_j2_next','right_j3_next','right_j4_next','right_j5_next','right_j6_next, eef_pose_x, eef_pose_y, eef_pose_z'])
+
+	reader.next()
+	reader.next()
+	reader = list(reader)
+
+	for i in range(len(reader)-1):
+		if float(reader[i][9]) == float(reader[i+1][9]):
+			joints = {'right_j0':float(reader[i+1][2]), 'right_j1':float(reader[i+1][3]), 'right_j2': float(reader[i+1][4]), 'right_j3': float(reader[i+1][5]), 'right_j4':float(reader[i+1][6]), 'right_j5':float(reader[i+1][7]), 'right_j6':float(reader[i+1][8])}
+			eef_pose = limb.joint_angles_to_cartesian_pose(joints)
+			writer.writerow([reader[i+1][2], reader[i+1][3], reader[i+1][4], reader[i+1][5], reader[i+1][6], reader[i+1][7], reader[i+1][8], eef_pose.position.x, eef_pose.position.y, eef_pose.position.z])
+		else:
+			joints = {'right_j0':float(reader[i][2]), 'right_j1':float(reader[i][3]), 'right_j2': float(reader[i][4]), 'right_j3': float(reader[i][5]), 'right_j4': float(reader[i][6]), 'right_j5':float(reader[i][7]), 'right_j6':float(reader[i][8])}
+			eef_pose = limb.joint_angles_to_cartesian_pose(joints)
+			writer.writerow([reader[i][2], reader[i][3], reader[i][4], reader[i][5], reader[i][6], reader[i][7], reader[i][8], eef_pose.position.x, eef_pose.position.y, eef_pose.position.z])
+
+generate_next_actions()
